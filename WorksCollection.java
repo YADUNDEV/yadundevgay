b346f2546944d146b501ece73b90e25b008cd52c
YADUNDEV <40615301+YADUNDEV@users.noreply.github.com>
2020-01-10 14:51:59 -0500

    no message

--- WorksCollection.java
+++ WorksCollection.java
@@ -88,3 +88,23 @@ function loadContent(work){
   }
   document.querySelector(".works-content").appendChild(wnode);
 }
+
+//WORK EXPANSION//
+var acc = document.getElementsByClassName("accordion");
+var i;
+for (i = 0; i < acc.length; i++)
+{
+  acc[i].addEventListener("click", function()
+  {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight)
+    {
+      panel.style.maxHeight = null;
+    }
+    else
+    {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
