5713ec1d7cd7053b99f78a196a212a4a8bee2f04
YADUNDEV <40615301+YADUNDEV@users.noreply.github.com>
2020-01-13 16:39:56 -0500

    no message

--- worksScrolling.js
+++ worksScrolling.js
@@ -1,5 +1,5 @@
 $('.works-content').infiniteScroll({
-  //path: '.pagination__next',
+  path: '.pagination__next',
   append: '.works-content-iFrame',
   status: '.scroller-status',
   hideNav: '.pagination'
